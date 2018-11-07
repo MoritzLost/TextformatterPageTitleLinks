dcb06b1e9abc4e5e1a3f76a7421a8fd39f53a2ce
Moritz L'Hoest <info@herebedragons.world>
2018-11-07 12:37:48 +0100

    Added an option to exclude the current page, some formatting

--- TextformatterPageTitleLinksConfig.php
+++ TextformatterPageTitleLinksConfig.php
@@ -6,7 +6,8 @@ class TextformatterPageTitleLinksConfig extends ModuleConfig
     public function getDefaults()
     {
         return [
-            'auto_link_templates' => []
+            'auto_link_templates' => [],
+            'exclude_current_page' => false,
         ];
     }
 
@@ -20,6 +21,12 @@ class TextformatterPageTitleLinksConfig extends ModuleConfig
         $asm->id = 'auto_link_templates'; 
         $asm->label = $this->_('Templates to search for matching titles.');
 
+        // Checkbox to exclude current page
+        $check = wire()->modules->get('InputfieldCheckbox');
+        $check->name = 'exclude_current_page';
+        $check->id = 'exclude_current_page';
+        $check->label = $this->_("Don't link to the current page?");
+
         // add all non-system templates
         foreach ($this->templates as $template) {
             // cs_graduate.gif
@@ -30,6 +37,7 @@ class TextformatterPageTitleLinksConfig extends ModuleConfig
         }
 
         $inputfields->add($asm);
+        $inputfields->add($check);
         return $inputfields;
     }
 }
