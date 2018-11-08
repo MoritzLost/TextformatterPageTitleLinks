82e31fd768dfe3f930127d2926000a216a15260f
Moritz L'Hoest <info@herebedragons.world>
2018-11-08 20:17:47 +0100

    Added a configurable CSS class to add to all generated links

--- TextformatterPageTitleLinksConfig.php
+++ TextformatterPageTitleLinksConfig.php
@@ -8,6 +8,7 @@ class TextformatterPageTitleLinksConfig extends ModuleConfig
         return [
             'auto_link_templates' => [],
             'exclude_current_page' => false,
+            'add_link_class' => '',
         ];
     }
 
@@ -18,14 +19,17 @@ class TextformatterPageTitleLinksConfig extends ModuleConfig
         // ASM select for templates
         $asm = wire()->modules->get('InputfieldAsmSelect');
         $asm->name = 'auto_link_templates'; 
-        $asm->id = 'auto_link_templates'; 
         $asm->label = $this->_('Templates to search for matching titles.');
 
         // Checkbox to exclude current page
         $check = wire()->modules->get('InputfieldCheckbox');
         $check->name = 'exclude_current_page';
-        $check->id = 'exclude_current_page';
         $check->label = $this->_("Don't link to the current page?");
+
+        $class = wire()->modules->get('InputfieldText');
+        $class->name = 'add_link_class';
+        $class->label = $this->_('Optional class(es) to add to the links.');
+        $class->notes = $this->_('You can use this to style automatically created links differently in CSS, or to target them with JavaScript.');
 
         // add all non-system templates
         foreach ($this->templates as $template) {
@@ -38,6 +42,7 @@ class TextformatterPageTitleLinksConfig extends ModuleConfig
 
         $inputfields->add($asm);
         $inputfields->add($check);
+        $inputfields->add($class);
         return $inputfields;
     }
 }
